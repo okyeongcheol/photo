05c81d3527a2d2d7351654d2a2a37992b474f1b3
okyeongcheol <105191977+okyeongcheol@users.noreply.github.com>
2022-07-06 14:40:52 +0900

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,71 @@
+$(function () {
+  $(".pu1").click(function () {
+    $(".popup1").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+  $(".close").click(function () {
+    $(".popup").fadeOut();
+  });//.pupup p a를 클릭했을때 이벤트
+
+  $(".pu2").click(function () {
+    $(".popup2").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu3").click(function () {
+    $(".popup3").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu4").click(function () {
+    $(".popup4").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu5").click(function () {
+    $(".popup5").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu6").click(function () {
+    $(".popup6").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu7").click(function () {
+    $(".popup7").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu8").click(function () {
+    $(".popup8").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu9").click(function () {
+    $(".popup9").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu10").click(function () {
+    $(".popup10").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu11").click(function () {
+    $(".popup11").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu12").click(function () {
+    $(".popup12").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu13").click(function () {
+    $(".popup13").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu14").click(function () {
+    $(".popup14").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu15").click(function () {
+    $(".popup15").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu16").click(function () {
+    $(".popup16").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+ 
+
+});//기본구조
